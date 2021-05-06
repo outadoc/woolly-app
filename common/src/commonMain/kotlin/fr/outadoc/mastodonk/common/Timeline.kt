76d366982b259f1d7c489e82bb21a48970b48565
Baptiste Candellier <baptiste@candellier.me>
2021-05-06 21:00:06 +0200

    Increase Gradle heap size

--- common/src/commonMain/kotlin/fr/outadoc/mastodonk/common/Timeline.kt
+++ common/src/commonMain/kotlin/fr/outadoc/mastodonk/common/Timeline.kt
@@ -13,7 +13,6 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.text.BasicText
 import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material.Card
 import androidx.compose.material.CircularProgressIndicator

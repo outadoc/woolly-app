ea4517e9b62eecb5cf7767fcdaf448072e5d4154
Baptiste Candellier <baptiste@candellier.me>
2021-05-08 18:53:07 +0200

    chore(timeline): use `by` keyword for di

--- common/src/commonMain/kotlin/fr/outadoc/woolly/common/feature/timeline/global/GlobalTimelineScreen.kt
+++ common/src/commonMain/kotlin/fr/outadoc/woolly/common/feature/timeline/global/GlobalTimelineScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import fr.outadoc.woolly.common.ui.Timeline
 import org.kodein.di.compose.LocalDI
 import org.kodein.di.instance
@@ -11,12 +12,12 @@ import org.kodein.di.instance
 @Composable
 fun GlobalTimelineScreen(insets: PaddingValues) {
     val di = LocalDI.current
-    val viewModel: GlobalTimelineViewModel by di.instance()
-    val currentState = viewModel.state.collectAsState()
+    val viewModel by di.instance<GlobalTimelineViewModel>()
+    val currentState by viewModel.state.collectAsState()
 
     LaunchedEffect(Unit) {
         viewModel.onPageOpen()
     }
 
-    Timeline(insets = insets, state = currentState.value)
+    Timeline(insets = insets, state = currentState)
 }
--- common/src/commonMain/kotlin/fr/outadoc/woolly/common/feature/timeline/local/LocalTimelineScreen.kt
+++ common/src/commonMain/kotlin/fr/outadoc/woolly/common/feature/timeline/local/LocalTimelineScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import fr.outadoc.woolly.common.ui.Timeline
 import org.kodein.di.compose.LocalDI
 import org.kodein.di.instance
@@ -11,12 +12,12 @@ import org.kodein.di.instance
 @Composable
 fun LocalTimelineScreen(insets: PaddingValues) {
     val di = LocalDI.current
-    val viewModel: LocalTimelineViewModel by di.instance()
-    val currentState = viewModel.state.collectAsState()
+    val viewModel by di.instance<LocalTimelineViewModel>()
+    val currentState by viewModel.state.collectAsState()
 
     LaunchedEffect(Unit) {
         viewModel.onPageOpen()
     }
 
-    Timeline(insets = insets, state = currentState.value)
+    Timeline(insets = insets, state = currentState)
 }

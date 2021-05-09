c1b74bab5f93bbac63a5b5814d36a18333e56436
Baptiste Candellier <baptiste@candellier.me>
2021-05-09 10:37:55 +0200

    chore(time): undo code cleanup

--- common/src/commonMain/kotlin/fr/outadoc/woolly/common/ui/Time.kt
+++ common/src/commonMain/kotlin/fr/outadoc/woolly/common/ui/Time.kt
@@ -6,7 +6,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import kotlinx.datetime.Instant
 import kotlin.math.truncate
-import kotlin.time.DurationUnit
 import kotlin.time.ExperimentalTime
 
 @OptIn(ExperimentalTime::class)
@@ -21,11 +20,11 @@ fun RelativeTime(
     val duration = currentTime - time
 
     val stringDuration = when {
-        duration.toDouble(DurationUnit.SECONDS) < 5.0 -> "Now"
-        duration.toDouble(DurationUnit.MINUTES) < 1.0 -> "${truncate(duration.toDouble(DurationUnit.SECONDS)).toInt()} s"
-        duration.toDouble(DurationUnit.HOURS) < 1.0 -> "${truncate(duration.toDouble(DurationUnit.MINUTES)).toInt()} m"
-        duration.toDouble(DurationUnit.DAYS) < 1.0 -> "${truncate(duration.toDouble(DurationUnit.DAYS)).toInt()} h"
-        else -> "${truncate(duration.toDouble(DurationUnit.DAYS)).toInt()} d"
+        duration.inSeconds < 5.0 -> "Now"
+        duration.inMinutes < 1.0 -> "${truncate(duration.inSeconds).toInt()} s"
+        duration.inHours < 1.0 -> "${truncate(duration.inMinutes).toInt()} m"
+        duration.inDays < 1.0 -> "${truncate(duration.inDays).toInt()} h"
+        else -> "${truncate(duration.inDays).toInt()} d"
     }
 
     Text(

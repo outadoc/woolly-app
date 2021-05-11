5a5133030935374882daef661ad581f049b0dfe1
Baptiste Candellier <baptiste@candellier.me>
2021-05-11 23:29:30 +0200

    fix(search): fix app bar background color

--- common/src/commonMain/kotlin/fr/outadoc/woolly/common/feature/search/SearchScreen.kt
+++ common/src/commonMain/kotlin/fr/outadoc/woolly/common/feature/search/SearchScreen.kt
@@ -4,22 +4,31 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.AppBarDefaults
+import androidx.compose.material.ContentAlpha
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
+import androidx.compose.material.LocalContentColor
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
 import androidx.compose.material.Tab
 import androidx.compose.material.TabRow
 import androidx.compose.material.Text
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.contentColorFor
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.primarySurface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import fr.outadoc.woolly.common.screen.AppScreen
 import fr.outadoc.woolly.common.ui.MainBottomNavigation
@@ -45,40 +54,56 @@ fun SearchScreen(onScreenSelected: (AppScreen) -> Unit) {
 
     Scaffold(
         topBar = {
-            Column {
-                OutlinedTextField(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(8.dp),
-                    value = searchTerm,
-                    onValueChange = { term -> searchTerm = term },
-                    leadingIcon = {
-                        Icon(Icons.Default.Search, "Search")
-                    },
-                    placeholder = {
-                        Text("Search for something…")
-                    },
-                    singleLine = true,
-                    trailingIcon = {
-                        if (searchTerm.isNotEmpty()) {
-                            IconButton(onClick = { searchTerm = "" }) {
-                                Icon(Icons.Default.Clear, "Clear search")
-                            }
-                        }
-                    }
-                )
-
-                if (searchTerm.isNotEmpty()) {
-                    TabRow(selectedTabIndex = tabs.indexOf(currentSubScreen)) {
-                        tabs.forEach { screen ->
-                            Tab(
-                                modifier = Modifier.height(48.dp),
-                                selected = currentSubScreen == screen,
-                                onClick = {
-                                    currentSubScreen = screen
+            Surface(
+                color = MaterialTheme.colors.primarySurface,
+                elevation = AppBarDefaults.TopAppBarElevation
+            ) {
+                Column {
+                    OutlinedTextField(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp),
+                        value = searchTerm,
+                        onValueChange = { term -> searchTerm = term },
+                        singleLine = true,
+                        leadingIcon = {
+                            Icon(Icons.Default.Search, "Search")
+                        },
+                        placeholder = {
+                            Text("Search for something…")
+                        },
+                        trailingIcon = {
+                            if (searchTerm.isNotEmpty()) {
+                                IconButton(onClick = { searchTerm = "" }) {
+                                    Icon(Icons.Default.Clear, "Clear search")
                                 }
-                            ) {
-                                Text(text = searchRes.getScreenTitle(screen))
+                            }
+                        },
+                        colors = TextFieldDefaults.outlinedTextFieldColors(
+                            cursorColor = LocalContentColor.current,
+                            leadingIconColor = LocalContentColor.current,
+                            placeholderColor = LocalContentColor.current,
+                            trailingIconColor = LocalContentColor.current,
+                            focusedBorderColor = LocalContentColor.current.copy(ContentAlpha.medium),
+                            unfocusedBorderColor = Color.Transparent
+                        )
+                    )
+
+                    if (searchTerm.isNotEmpty()) {
+                        TabRow(
+                            selectedTabIndex = tabs.indexOf(currentSubScreen),
+                            backgroundColor = MaterialTheme.colors.primarySurface
+                        ) {
+                            tabs.forEach { screen ->
+                                Tab(
+                                    modifier = Modifier.height(48.dp),
+                                    selected = currentSubScreen == screen,
+                                    onClick = {
+                                        currentSubScreen = screen
+                                    }
+                                ) {
+                                    Text(text = searchRes.getScreenTitle(screen))
+                                }
                             }
                         }
                     }

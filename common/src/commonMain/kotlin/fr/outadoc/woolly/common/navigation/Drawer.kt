fc36184b33524e2fd267c7c8c8c2c42e5e4edca4
Baptiste Candellier <baptiste@candellier.me>
2021-05-18 14:05:25 +0200

    fix(drawer): make nav container scrollable

--- common/src/commonMain/kotlin/fr/outadoc/woolly/common/navigation/Drawer.kt
+++ common/src/commonMain/kotlin/fr/outadoc/woolly/common/navigation/Drawer.kt
@@ -10,6 +10,8 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.DrawerState
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Icon
@@ -85,7 +87,13 @@ fun MainAppDrawer(
             )
         }
 
-        Column(modifier = Modifier.padding(vertical = 16.dp)) {
+        val scrollState = rememberScrollState()
+
+        Column(
+            modifier = Modifier
+                .padding(vertical = 16.dp)
+                .verticalScroll(scrollState)
+        ) {
             screens.forEach { screen ->
                 DrawerItem(
                     title = { Text(res.getScreenTitle(screen)) },

4fb1b124b98c179fb5117b0554b3053e593dd340
Baptiste Candellier <baptiste@candellier.me>
2021-05-21 20:00:09 +0200

    fix(theme): set correct top/bottom bar color

--- common/src/commonMain/kotlin/fr/outadoc/woolly/common/ui/Theme.kt
+++ common/src/commonMain/kotlin/fr/outadoc/woolly/common/ui/Theme.kt
@@ -1,10 +1,14 @@
 package fr.outadoc.woolly.common.ui
 
+import androidx.compose.material.AppBarDefaults
+import androidx.compose.material.LocalElevationOverlay
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 @Composable
@@ -16,17 +20,29 @@ fun AppTheme(
         colors = if (isDarkMode) darkColors() else lightColors(),
     ) {
         val systemUiController = rememberSystemUiController()
-        val systemBarsColor =
+
+        val baseColor =
             if (isDarkMode) MaterialTheme.colors.surface
             else MaterialTheme.colors.primaryVariant
 
+        val topColor = baseColor.elevated(AppBarDefaults.TopAppBarElevation)
+        val bottomColor = baseColor.elevated(AppBarDefaults.BottomAppBarElevation)
+
         SideEffect {
-            systemUiController.setSystemBarsColor(
-                color = systemBarsColor,
-                darkIcons = false
-            )
+            systemUiController.setStatusBarColor(topColor)
+            systemUiController.setNavigationBarColor(bottomColor)
         }
 
         content()
+    }
+}
+
+@Composable
+private fun Color.elevated(elevation: Dp): Color {
+    val elevationOverlay = LocalElevationOverlay.current
+    return if (this == MaterialTheme.colors.surface && elevationOverlay != null) {
+        elevationOverlay.apply(this, elevation)
+    } else {
+        this
     }
 }

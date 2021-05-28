6a278122915dd87cc4768356ae801c1f5536cac0
Baptiste Candellier <baptiste@candellier.me>
2021-05-28 21:08:42 +0200

    feat(status): support content warnings

--- common/src/commonMain/kotlin/fr/outadoc/woolly/common/feature/status/ui/Status.kt
+++ common/src/commonMain/kotlin/fr/outadoc/woolly/common/feature/status/ui/Status.kt
@@ -1,5 +1,10 @@
 package fr.outadoc.woolly.common.feature.status.ui
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.expandIn
+import androidx.compose.animation.shrinkOut
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
@@ -11,16 +16,24 @@ import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.Card
 import androidx.compose.material.Icon
 import androidx.compose.material.IconToggleButton
 import androidx.compose.material.LocalContentColor
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.ArrowDropUp
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Repeat
 import androidx.compose.material.icons.filled.Reply
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -99,7 +112,7 @@ fun StatusWithActions(
             currentTime = currentTime
         )
 
-        StatusBody(status = status)
+        StatusBodyOrWarning(status = status)
 
         if (status.mediaAttachments.isNotEmpty()) {
             StatusMediaGrid(
@@ -125,6 +138,84 @@ fun StatusWithActions(
                 status = status,
                 onStatusAction = onStatusAction
             )
+        }
+    }
+}
+
+@OptIn(ExperimentalAnimationApi::class)
+@Composable
+fun StatusBodyOrWarning(
+    modifier: Modifier = Modifier,
+    status: Status
+) {
+    if (status.contentWarningText.isNotBlank()) {
+        var isCollapsed by remember { mutableStateOf(true) }
+
+        ContentWarningBanner(
+            modifier = Modifier
+                .padding(vertical = 8.dp)
+                .fillMaxWidth(),
+            contentWarning = status.contentWarningText,
+            isCollapsed = isCollapsed,
+            onToggle = {
+                isCollapsed = !isCollapsed
+            }
+        )
+
+        AnimatedVisibility(visible = !isCollapsed) {
+            StatusBody(
+                modifier = modifier.padding(top = 8.dp),
+                status = status
+            )
+        }
+
+    } else {
+        StatusBody(
+            modifier = modifier,
+            status = status
+        )
+    }
+}
+
+@Composable
+fun ContentWarningBanner(
+    modifier: Modifier = Modifier,
+    contentWarning: String,
+    isCollapsed: Boolean,
+    onToggle: () -> Unit
+) {
+    Card(modifier = modifier.clickable { onToggle() }) {
+        Row(
+            modifier = Modifier.padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Icon(
+                    Icons.Default.Info,
+                    contentDescription = "Content warning",
+                    modifier = Modifier.padding(end = 16.dp)
+                )
+
+                Text(
+                    modifier = Modifier.padding(end = 8.dp),
+                    text = contentWarning,
+                    style = MaterialTheme.typography.body1,
+                    overflow = TextOverflow.Ellipsis
+                )
+            }
+
+            if (isCollapsed) {
+                Icon(
+                    Icons.Default.ArrowDropDown,
+                    contentDescription = "Expand post"
+                )
+            } else {
+                Icon(
+                    Icons.Default.ArrowDropUp,
+                    contentDescription = "Collapse post"
+                )
+            }
         }
     }
 }

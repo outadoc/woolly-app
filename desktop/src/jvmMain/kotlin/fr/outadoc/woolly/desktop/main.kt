f0ae43ada56439a9d37050d83a0c0222fef9a988
Baptiste Candellier <outadoc@gmail.com>
2021-07-01 22:10:33 +0200

    feat(desktop): use new composable Window API

--- desktop/src/jvmMain/kotlin/fr/outadoc/woolly/desktop/main.kt
+++ desktop/src/jvmMain/kotlin/fr/outadoc/woolly/desktop/main.kt
@@ -1,10 +1,14 @@
 package fr.outadoc.woolly.desktop
 
-import androidx.compose.desktop.Window
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.platform.LocalUriHandler
-import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowSize
+import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 import fr.outadoc.woolly.common.App
 import fr.outadoc.woolly.common.feature.preference.DesktopPreferenceRepositoryImpl
 import fr.outadoc.woolly.common.feature.preference.PreferenceRepository
@@ -17,29 +21,34 @@ import org.kodein.di.bindSingleton
 import org.kodein.di.compose.withDI
 import org.kodein.di.instance
 
-fun main() = Window(
-    title = "Woolly",
-    size = IntSize(width = 850, height = 750)
-) {
+@OptIn(ExperimentalComposeUiApi::class)
+fun main() = application {
     DesktopApp()
-}
-
-@Composable
-private fun DesktopApp() = withDI(di) {
-    CompositionLocalProvider(
-        LocalUriHandler provides DesktopUriHandler()
-    ) {
-        App()
-    }
 }
 
 @OptIn(DelicateCoroutinesApi::class)
 private val di = DI {
 
-    bindSingleton { Json {} }
+    bindSingleton { Json.Default }
     bindSingleton<CoroutineScope> { GlobalScope }
 
     bindSingleton<PreferenceRepository> {
         DesktopPreferenceRepositoryImpl(instance())
+    }
+}
+
+@ExperimentalComposeUiApi
+@Composable
+private fun DesktopApp() = withDI(di) {
+    CompositionLocalProvider(
+        LocalUriHandler provides DesktopUriHandler()
+    ) {
+        val windowState = rememberWindowState(
+            size = WindowSize(width = 850.dp, height = 750.dp)
+        )
+
+        Window(title = "Woolly", state = windowState) {
+            App()
+        }
     }
 }

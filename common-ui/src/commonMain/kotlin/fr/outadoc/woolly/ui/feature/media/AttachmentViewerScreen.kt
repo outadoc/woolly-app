1fa289e4a82f7a3bfe1ba99d290778a4989a19f1
Baptiste Candellier <baptiste@candellier.me>
2021-08-28 16:11:14 +0200

    feat(media): support some zoom on desktop

--- common-ui/src/commonMain/kotlin/fr/outadoc/woolly/ui/feature/media/AttachmentViewerScreen.kt
+++ common-ui/src/commonMain/kotlin/fr/outadoc/woolly/ui/feature/media/AttachmentViewerScreen.kt
@@ -1,6 +1,9 @@
 package fr.outadoc.woolly.ui.feature.media
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.gestures.detectTransformGestures
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -23,6 +26,7 @@ fun ImageViewerScreen(image: ImageAttachment) {
     )
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun PinchToZoomImage(
     modifier: Modifier = Modifier,
@@ -30,13 +34,21 @@ fun PinchToZoomImage(
     contentDescription: String?
 ) {
     var scale by remember { mutableStateOf(1f) }
-    var rotationState by remember { mutableStateOf(1f) }
+    var rotationState by remember { mutableStateOf(0f) }
     var translationX by remember { mutableStateOf(0f) }
     var translationY by remember { mutableStateOf(0f) }
 
     Column(
         modifier = modifier
             .fillMaxSize()
+            .combinedClickable(
+                indication = null,
+                interactionSource =  remember { MutableInteractionSource() },
+                onClick = {},
+                onDoubleClick = {
+                    scale = if (scale == 2f) 1f else 2f
+                }
+            )
             .pointerInput(Unit) {
                 detectTransformGestures { _, pan, zoom, rotation ->
                     scale *= zoom

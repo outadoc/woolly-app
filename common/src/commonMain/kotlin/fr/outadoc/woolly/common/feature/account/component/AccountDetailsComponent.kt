10d5f1067c3b7ca52b6c147473d96489cd11a846
Baptiste Candellier <baptiste@candellier.me>
2021-09-13 21:30:52 +0200

    feat(status): implement follow/unfollow button

--- common/src/commonMain/kotlin/fr/outadoc/woolly/common/feature/account/component/AccountDetailsComponent.kt
+++ common/src/commonMain/kotlin/fr/outadoc/woolly/common/feature/account/component/AccountDetailsComponent.kt
@@ -5,7 +5,9 @@ import fr.outadoc.mastodonk.api.entity.Account
 import fr.outadoc.mastodonk.api.entity.Relationship
 import fr.outadoc.woolly.common.feature.client.MastodonClientProvider
 import fr.outadoc.woolly.common.getScope
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 class AccountDetailsComponent(
@@ -13,81 +15,90 @@ class AccountDetailsComponent(
     private val clientProvider: MastodonClientProvider
 ) : ComponentContext by componentContext {
 
-    sealed class State(
-        open val isLoading: Boolean
-    ) {
+    sealed class State {
+        abstract val isLoading: Boolean
+        abstract val accountId: String?
+
         data class Initial(
-            override val isLoading: Boolean = false
-        ) : State(isLoading)
+            override val isLoading: Boolean = true,
+            override val accountId: String? = null
+        ) : State()
 
         data class Error(
             val exception: Exception? = null,
-            override val isLoading: Boolean = false
-        ) : State(isLoading)
+            override val isLoading: Boolean = false,
+            override val accountId: String? = null
+        ) : State()
 
         data class LoadedAccount(
             val account: Account,
             val relationship: Relationship,
             override val isLoading: Boolean = false
-        ) : State(isLoading)
+        ) : State() {
+
+            override val accountId: String
+                get() = account.accountId
+        }
     }
 
     private val componentScope = getScope()
 
-    private val accountIdFlow = MutableSharedFlow<String>(replay = 1)
+    private val _state = MutableStateFlow<State>(State.Initial())
+    val state: Flow<State> = _state.asStateFlow()
 
-    val state: Flow<State> = flow {
-        var currentState: State = State.Initial(isLoading = true)
-
-        combine(clientProvider.mastodonClient, accountIdFlow) { client, accountId ->
-            emit(currentState.copy(isLoading = true))
-
-            val nextState = if (client == null) State.Error()
-            else {
-                val account = client.accounts.getAccount(accountId)
-                    ?: return@combine State.Error()
-
-                val relationship = client.accounts.getRelationships(listOf(accountId))
-                    ?.firstOrNull()
-                    ?: return@combine State.Error()
-
-                State.LoadedAccount(
-                    account = account,
-                    relationship = relationship
-                )
-            }
-
-            currentState = nextState
-            emit(nextState)
-
-        }.collect()
-    }
-
-    fun State.copy(isLoading: Boolean): State {
+    fun State.copy(isLoading: Boolean, accountId: String?): State {
         return when (this) {
-            is State.Error -> this.copy(isLoading = isLoading)
-            is State.Initial -> this.copy(isLoading = isLoading)
+            is State.Error -> this.copy(isLoading = isLoading, accountId = accountId)
+            is State.Initial -> this.copy(isLoading = isLoading, accountId = accountId)
             is State.LoadedAccount -> this.copy(isLoading = isLoading)
         }
     }
 
     fun loadAccount(accountId: String) {
-        accountIdFlow.tryEmit(accountId)
-    }
+        val client = clientProvider.mastodonClient.value ?: return
 
-    fun refresh() {
-        accountIdFlow.replayCache.lastOrNull()?.let { currentId ->
-            accountIdFlow.tryEmit(currentId)
+        componentScope.launch {
+            _state.value = _state.value.copy(
+                isLoading = true,
+                accountId = accountId
+            )
+
+            val account = client.accounts.getAccount(accountId)
+            val relationship = client.accounts.getRelationships(listOf(accountId))?.firstOrNull()
+
+            _state.value =
+                if (account == null || relationship == null) {
+                    State.Error(accountId = accountId)
+                } else State.LoadedAccount(
+                    account = account,
+                    relationship = relationship
+                )
         }
     }
 
+    fun refresh() {
+        val currentAccountId = _state.value.accountId ?: return
+        loadAccount(currentAccountId)
+    }
+
     fun onFollowClick(follow: Boolean) {
+        val currentState = _state.value as? State.LoadedAccount ?: return
+        val client = clientProvider.mastodonClient.value ?: return
+
+        _state.value = currentState.copy(
+            relationship = currentState.relationship.copy(
+                isFollowing = follow
+            )
+        )
+
         componentScope.launch {
-            clientProvider.mastodonClient.value?.accounts?.let { api ->
+            try {
                 when (follow) {
-                    true -> TODO()
-                    false -> TODO()
+                    true -> client.accounts.followAccount(accountId = currentState.accountId)
+                    false -> client.accounts.unfollowAccount(accountId = currentState.accountId)
                 }
+            } catch (e: Exception) {
+                _state.value = currentState
             }
         }
     }

e00d128ae14c6b81fbc1eb08bf0e853c6021f4bc
Baptiste Candellier <baptiste.candellier@bedrockstreaming.com>
2021-09-15 20:57:27 +0200

    feat(desktop): use system theme on macos

--- app-desktop/build.gradle.kts
+++ app-desktop/build.gradle.kts
@@ -38,13 +38,36 @@ compose.desktop {
         mainClass = "fr.outadoc.woolly.desktop.Woolly"
 
         nativeDistributions {
-            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            targetFormats(
+                TargetFormat.Dmg,
+                TargetFormat.Msi,
+                TargetFormat.Deb,
+                TargetFormat.Rpm
+            )
+
             packageName = "Woolly"
             description = "Desktop client for Mastodon"
             packageVersion = project.version as String
             version = project.version as String
             copyright = "© 2021 Baptiste Candellier"
             vendor = "Baptiste Candellier"
+
+            jvmArgs(
+                // Auto-switch dark theme on macOS
+                "-Dapple.awt.application.appearance=system"
+            )
+
+            macOS {
+                bundleID = "fr.outadoc.woolly.desktop"
+            }
+
+            windows {
+                upgradeUuid = "22afac0d-e867-47ed-a874-875e56c58b9e"
+            }
+
+            linux {
+                debMaintainer = "baptiste@candellier.me"
+            }
         }
     }
 }

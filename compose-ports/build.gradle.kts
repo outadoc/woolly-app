aa97b8581e2a7e786a726f0095f5b2f4808b19f2
Baptiste Candellier <baptiste@candellier.me>
2021-10-17 15:02:30 +0200

    Extract ports into own module

---
+++ compose-ports/build.gradle.kts
@@ -0,0 +1,71 @@
+import org.jetbrains.compose.compose
+
+plugins {
+    kotlin("multiplatform")
+    id("org.jetbrains.compose")
+    id("com.android.library")
+    id("kotlin-parcelize")
+}
+
+kotlin {
+    android()
+
+    jvm("desktop") {
+        compilations.all {
+            kotlinOptions.jvmTarget = "11"
+        }
+    }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(compose.foundation)
+                implementation(compose.runtime)
+                implementation(compose.material)
+                implementation(compose.ui)
+
+                implementation(libs.kotlinx.coroutines)
+                implementation(libs.androidx.paging)
+            }
+        }
+
+        val jvmMain by creating {
+            dependsOn(commonMain)
+        }
+
+        val androidMain by getting {
+            dependsOn(jvmMain)
+            dependencies {
+                implementation(libs.androidx.appcompat)
+                implementation(libs.androidx.core)
+            }
+        }
+
+        val desktopMain by getting {
+            dependsOn(jvmMain)
+        }
+    }
+}
+
+android {
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+
+    compileSdk = 31
+    defaultConfig {
+        minSdk = 24
+        targetSdk = 31
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.0.0-rc01"
+    }
+}

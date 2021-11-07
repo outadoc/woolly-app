70154e2410c98239830e3b931043454b4496144d
Baptiste Candellier <baptiste@candellier.me>
2021-11-07 19:40:24 +0100

    fix(build): fix duplicate class on android

--- compose-ports/src/commonMain/kotlin/fr/outadoc/woolly/backports/compose/ui/util/MathHelpers.kt
+++ compose-ports/src/commonMain/kotlin/fr/outadoc/woolly/backports/compose/ui/util/MathHelpers.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package androidx.compose.ui.util
+package fr.outadoc.woolly.backports.compose.ui.util
 
 import kotlin.math.roundToInt
 import kotlin.math.roundToLong

0a2c150471f5fb10ddc3c88f3c8748afd0ce7641
Baptiste Candellier <baptiste@candellier.me>
2021-11-24 13:50:11 +0100

    Wrap Material3 theme inside Material2 theme

--- ui/src/commonMain/kotlin/fr/outadoc/woolly/ui/common/WoollyTheme.kt
+++ ui/src/commonMain/kotlin/fr/outadoc/woolly/ui/common/WoollyTheme.kt
@@ -1,5 +1,6 @@
 package fr.outadoc.woolly.ui.common
 
+import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 
@@ -8,8 +9,32 @@ fun WoollyTheme(
     isDarkMode: Boolean,
     content: @Composable () -> Unit
 ) {
-    MaterialTheme(
-        colorScheme = if (isDarkMode) woollyDarkColors() else woollyLightColors(),
-        content = content,
-    )
+    val colorScheme = if (isDarkMode) woollyDarkColors() else woollyLightColors()
+
+    // Wrap Material3 theme inside Material2 theme until all components are available
+    androidx.compose.material.MaterialTheme(
+        colors = colorScheme.toMaterial2Colors(isDarkMode)
+    ) {
+        MaterialTheme(
+            colorScheme = colorScheme,
+            content = content
+        )
+    }
 }
+
+private fun ColorScheme.toMaterial2Colors(isDarkMode: Boolean) =
+    androidx.compose.material.Colors(
+        isLight = !isDarkMode,
+        primary = primary,
+        primaryVariant = primaryContainer,
+        onPrimary = onPrimary,
+        secondary = secondary,
+        secondaryVariant = secondaryContainer,
+        onSecondary = onSecondary,
+        background = background,
+        onBackground = onBackground,
+        surface = surface,
+        onSurface = onSurface,
+        error = error,
+        onError = onError
+    )
